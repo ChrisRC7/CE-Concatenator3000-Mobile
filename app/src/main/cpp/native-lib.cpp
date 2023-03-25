dc9a30fce790ff8412d59bb838c20619f8e1070f
ChrisRC7 <chrisrc7@live.com>
2023-03-25 12:22:07 -0600

    Concatenation

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,23 @@
 #include <jni.h>
 #include <string>
+using namespace  std;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ceconcatenator3000_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject/* this */,
+        jstring palabra1,
+        jstring palabra2){
+    const char* p1= env->GetStringUTFChars(palabra1, nullptr);
+    const char* p2= env->GetStringUTFChars(palabra2, nullptr);
+
+    std::string resultado = std::string(p1) + "" + std::string (p2);
+
+    env->ReleaseStringUTFChars(palabra1, p1);
+    env->ReleaseStringUTFChars(palabra2, p2);
+
+    return env->NewStringUTF(resultado.c_str());
+
+
+
 }
--- app/src/main/java/com/ceconcatenator3000/MainActivity.java
+++ app/src/main/java/com/ceconcatenator3000/MainActivity.java
@@ -3,7 +3,11 @@ package com.ceconcatenator3000;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ceconcatenator3000.databinding.ActivityMainBinding;
 
@@ -14,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("ceconcatenator3000");
     }
 
+    private EditText editText1;
+    private EditText editText2;
+    private TextView textViewResult;
+    private Button buttonConcatenate;
+
     private ActivityMainBinding binding;
 
     @Override
@@ -23,14 +32,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        editText1= findViewById(R.id.editText1);
+        editText2= findViewById(R.id.editText2);
+        textViewResult= findViewById(R.id.textResult);
+        buttonConcatenate= findViewById(R.id.button);
+
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        /*TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());*/
+
+        buttonConcatenate.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View view) {
+                String palabra1 = editText1.getText().toString();
+                String palabra2 = editText2.getText().toString();
+
+                String resultado = stringFromJNI(palabra1, palabra2);
+
+                //Toast.makeText(MainActivity.this, resultado, Toast.LENGTH_SHORT).show();
+                textViewResult.setText(resultado);
+            }
+        });
     }
 
     /**
      * A native method that is implemented by the 'ceconcatenator3000' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String stringFromJNI(String palabra1, String palabra2);
 }
